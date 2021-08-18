89bfc74f962d3aff026d8b0ef3ccb4989294e14d
Arshid Baba <arshidbaba93@gmail.com>
2021-08-19 00:06:12 +0530

    Registered models with admin

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Category)
+
+class CatAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+    ]
+
+@admin.register(models.Quizzes)
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 
+        'title',
+    ]
+
+class AnswerInlineModel(admin.TabularInline):
+    model = models.Answer
+    fields = [
+        'answer_text',
+        'is_right'
+    ]
